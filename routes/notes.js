2e3ff7e8516786b806e2a3a01b159b920e2ff89a
Maverick Wolfe <maverick.m.wolfe@gmail.com>
2024-02-07 09:35:41 -0700

    added api route for notes, created helper files for working with the file system

---
+++ routes/notes.js
@@ -0,0 +1,30 @@
+const notes = require("express").Router();
+const { v4: uuidv4 } = require("uuid");
+const { readFromFile, readAndAppend, writeToFile } = require("../helpers/fsUtils");
+
+// GET Route for retrieving all the notes
+notes.get("/", (req, res) => {
+  readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)));
+});
+
+// POST route for a new note
+notes.post("/", (req, res) => {
+  console.log(req.body);
+
+  const { title, text } = req.body;
+
+  if (req.body) {
+    const newNote = {
+      title,
+      text,
+      note_id: uuidv4(),
+    };
+
+    readAndAppend(newNote, "./db/notes.json");
+    res.json(`Note added successfully`);
+  } else {
+    res.error("Error in adding note");
+  }
+});
+
+module.exports = notes;
